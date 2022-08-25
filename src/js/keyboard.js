da9a6263ff59762faa2073d9f0097d171bfbc33e
DESKTOP-3FIHK6R\skswo <kieun.kwon@moyusnvc.com>
2022-08-25 22:29:53 +0900

    한글 적용
    - 3d가 작동할 수 있도록 관련 코드 수정
    - 기존 주석의 설명이 이상한 부분 수정
    - 개발자 도구 사용할 수 있도록 F12키는 금지 해제

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -5,10 +5,15 @@ export class Keyboard {
   #keyboardEl;
   #fnKeyArr;
   #inputEl;
+  #language;
+  #convertKeyEl;
+  #hangul;
 
   constructor() {
     this.assignElement();
     this.addEvent();
+    this.#hangul = require("hangul-js");
+    this.#language = "eng";
     this.#fnKeyArr = [
       "Tab",
       "AltLeft",
@@ -23,7 +28,6 @@ export class Keyboard {
       "F9",
       "F10",
       "F11",
-      "F12",
     ];
   }
 
@@ -34,6 +38,7 @@ export class Keyboard {
     this.#fontSelectEl = this.#containerEl.querySelector("#font");
     this.#keyboardEl = this.#containerEl.querySelector("#keyboard");
     this.#inputEl = this.#containerEl.querySelector(".input");
+    this.#convertKeyEl = this.#containerEl.querySelector(".convert-key");
   }
 
   // 이벤트 연결 함수
@@ -50,17 +55,45 @@ export class Keyboard {
       this.#onMouseDown.bind(this)
     );
     document.addEventListener("mouseup", this.#onMouseUp.bind(this));
+    this.#convertKeyEl.addEventListener(
+      "click",
+      this.#onChangeLanguage.bind(this)
+    );
 
     // 사용자 정의 이벤트 함수
     this.#inputEl.addEventListener("inputkey", this.#onWriteKey.bind(this));
   }
   #onMouseDown(event) {
-    event.target.closest("div.key")?.classList.add("active");
+    event.target.closest("div.key-wrap")?.classList.add("active");
+  }
+  // 한/영 키를 눌렀을 때 발생하는 이벤트 함수
+  #onChangeLanguage() {
+    const viewEl = this.#convertKeyEl.querySelector(".view");
+    this.#language = viewEl.dataset.languageto;
+    const children = this.#convertKeyEl.children;
+    children[0].classList.toggle("view");
+    children[1].classList.toggle("view");
+    const root = document.querySelector(":root");
+    // 한글과 영어로 변경됐을 때 위치와 회전율에 해당하는 변수를 수정
+    if (this.#language === "kor") {
+      root.style.setProperty("--front", 0);
+      root.style.setProperty("--back", 1);
+      root.style.setProperty("--front-rotate", "180deg");
+      root.style.setProperty("--back-rotate", "0deg");
+    } else if (this.#language === "eng") {
+      root.style.setProperty("--front", 1);
+      root.style.setProperty("--back", 0);
+      root.style.setProperty("--front-rotate", "360deg");
+      root.style.setProperty("--back-rotate", "180deg");
+    }
   }
   // key에 active 효과가 있는지 검사해서 inputkey라는 사용자 정의 함수를 호출
   #onMouseUp(event) {
-    const keyEl = event.target.closest("div.key");
-    const isActive = keyEl?.classList.contains("active");
+    const keyWrapEl = event.target.closest("div.key-wrap");
+    const keyEl = keyWrapEl?.querySelector(
+      `.key-${this.#language === "eng" ? "front" : "back"}`
+    );
+    const isActive = keyWrapEl?.classList.contains("active");
     const val = keyEl?.dataset.val;
     if (isActive && !!val) {
       this.#inputEl.dispatchEvent(
@@ -78,7 +111,11 @@ export class Keyboard {
     } else if (keyValue === "Space") {
       this.#inputEl.value = this.#inputEl.value + " ";
     } else {
-      this.#inputEl.value += keyValue;
+      const hangulProsessing = this.#hangul.assemble([
+        ...this.#inputEl.value,
+        keyValue,
+      ]);
+      this.#inputEl.value = hangulProsessing;
     }
   }
   #resetKeyState() {
@@ -110,16 +147,17 @@ export class Keyboard {
     });
   }
   #getKeyActionState(code) {
-    const keySelect = this.#keyboardEl.querySelector(`[data-code=${code}]`);
-    // f1~f12까지의 키 입력에 대해 오류를 방지하기 위한 조건문
-    if (keySelect === null) {
+    const tmpEl = this.#keyboardEl.querySelector(`[data-code=${code}]`);
+    const keySelect = tmpEl?.closest(".key-wrap");
+    // f1~f12 등 키 입력에 대해 오류를 방지하기 위한 조건문
+    if (keySelect === null || keySelect === undefined) {
       return { isContain: null, keySelect };
     }
     const isContain = keySelect.classList?.contains("active");
     return { isContain, keySelect };
   }
 
-  // 키보드의 입력을 받는 함수
+  // 키보드의 입력을 받아서 active 효과를 주는 함수
   #onKeyDown(event) {
     const { isContain, keySelect } = this.#getKeyActionState(event.code);
     if (isContain === null) return;
